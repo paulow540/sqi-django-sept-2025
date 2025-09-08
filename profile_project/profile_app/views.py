325fefc8a46a9a3c343de468329a409233b32ae5
paulow540 <halleluyahpaul@gmail.com>
2025-09-08 09:53:28 +0100

    Add template inheritance

--- profile_project/profile_app/views.py
+++ profile_project/profile_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, "profile_app/index.html")
+    return render(request, "profile_app/home.html")
 
 
 def hobbies(request):

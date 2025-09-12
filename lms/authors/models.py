af3ef49fd7af02b8fd1d116099ba010e5b6c5d88
paulow540 <halleluyahpaul@gmail.com>
2025-09-12 17:08:07 +0100

    Add lms models

--- lms/authors/models.py
+++ lms/authors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+
+    # class Meta:
+    #     table_name = "authors"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- lms/library/models.py
+++ lms/library/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from authors.models import Author
+
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    number_of_pages = models.PositiveIntegerField()
+    published_on = models.DateTimeField()
+
+
+

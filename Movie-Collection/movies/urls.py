2b4535394e7280e94fffe57f271e1e46319be9b0
paulow540 <halleluyahpaul@gmail.com>
2025-09-22 17:40:05 +0100

    Add the Update and Delete function to the movieapp

--- Movie-Collection/movies/urls.py
+++ Movie-Collection/movies/urls.py
@@ -1,12 +1,13 @@
+# movies/urls.py
 from django.urls import path
 from . import views
 
 app_name = 'movies'
 
 urlpatterns = [
-    path('', views.MovieListView.as_view(), name='movie-list'),
-    path('movies/create/', views.MovieCreateView.as_view(), name='movie-create'),
-    path('movies/<int:pk>/', views.MovieDetailView.as_view(), name='movie-detail'),
-    path('movies/<int:pk>/edit/', views.MovieUpdateView.as_view(), name='movie-edit'),
-    path('movies/<int:pk>/delete/', views.MovieDeleteView.as_view(), name='movie-delete'),
+    path('', views.movie_list, name='movie-list'),
+    path('movies/create/', views.movie_create, name='movie-create'),
+    path('movies/<int:pk>/', views.movie_detail, name='movie-detail'),
+    path('movies/<int:pk>/edit/', views.movie_edit, name='movie-edit'),
+    path('movies/<int:pk>/delete/', views.movie_delete, name='movie-delete'),
 ]
--- Movie-Collection/movies/views.py
+++ Movie-Collection/movies/views.py
@@ -1,38 +1,85 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+# movies/views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.urls import reverse
 from .models import Movie
 from .forms import MovieForm
 
-
-class MovieListView(ListView):
-    model = Movie
-    template_name = 'movies/movie_list.html'
-    context_object_name = 'movies'
-    paginate_by = 12
+PAGINATE_BY = 12
 
 
-class MovieDetailView(DetailView):
-    model = Movie
-    template_name = 'movies/movie_detail.html'
-    context_object_name = 'movie'
+def movie_list(request):
+    """List all movies with pagination."""
+    qs = Movie.objects.all().order_by('-release_date', 'title')
+    paginator = Paginator(qs, PAGINATE_BY)
+    page = request.GET.get('page', 1)
+
+    try:
+        movies = paginator.page(page)
+    except PageNotAnInteger:
+        movies = paginator.page(1)
+    except EmptyPage:
+        movies = paginator.page(paginator.num_pages)
+
+    context = {
+        'movies': movies,
+        'is_paginated': movies.has_other_pages(),
+        'page_obj': movies,
+        'paginator': paginator,
+    }
+    return render(request, 'movies/movie_list.html', context)
 
 
-class MovieCreateView(CreateView):
-    model = Movie
-    form_class = MovieForm
-    template_name = 'movies/movie_form.html'
+def movie_detail(request, pk):
+    """Show details for a single movie."""
+    movie = get_object_or_404(Movie, pk=pk)
+    return render(request, 'movies/movie_detail.html', {'movie': movie})
 
 
-class MovieUpdateView(UpdateView):
-    model = Movie
-    form_class = MovieForm
-    template_name = 'movies/movie_form.html'
+def movie_create(request):
+    """Create a new movie (handles file upload)."""
+    if request.method == 'POST':
+        form = MovieForm(request.POST, request.FILES)
+        if form.is_valid():
+            movie = form.save()
+            return redirect(movie.get_absolute_url())
+    else:
+        form = MovieForm()
+
+    return render(request, 'movies/movie_form.html', {'form': form})
 
 
-class MovieDeleteView(DeleteView):
-    model = Movie
-    template_name = 'movies/movie_confirm_delete.html'
-    success_url = reverse_lazy('movies:movie-list')
+def movie_edit(request, pk):
+    """Edit an existing movie."""
+    movie = get_object_or_404(Movie, pk=pk)
+
+    if request.method == 'POST':
+        form = MovieForm(request.POST, request.FILES, instance=movie)
+        if form.is_valid():
+            movie = form.save()
+            return redirect(movie.get_absolute_url())
+    else:
+        form = MovieForm(instance=movie)
+
+    return render(request, 'movies/movie_form.html', {'form': form, 'movie': movie})
+
+
+def movie_delete(request, pk):
+    """
+    Confirm-and-delete view.
+    GET -> show confirmation template
+    POST -> delete and redirect to list
+    """
+    movie = get_object_or_404(Movie, pk=pk)
+
+    if request.method == 'POST':
+        # If you want to also delete the poster file from disk, you can do it here:
+        # if movie.poster and movie.poster.path:
+        #     try:
+        #         os.remove(movie.poster.path)
+        #     except Exception:
+        #         pass
+        movie.delete()
+        return redirect(reverse('movies:movie-list'))
+
+    return render(request, 'movies/movie_confirm_delete.html', {'object': movie})
